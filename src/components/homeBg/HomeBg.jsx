a58e5b105256cb923cbed4c4d4274aa2f04263f9
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2024-08-01 09:10:17 +0100

    updated images

--- src/components/homeBg/HomeBg.jsx
+++ src/components/homeBg/HomeBg.jsx
@@ -1,18 +1,22 @@
 import BgImg from '../bgImage/BgImg'
+import logo1 from '../../assets/Images/bg-1A.jpg'
+import logo2 from '../../assets/Images/bg-1B.jpg'
+import logo3 from '../../assets/Images/bg-1C.jpg'
 
 function HomeBg() {
 
     const images = [
-        "src/assets/Images/bg-1A.jpg",
-        "src/assets/Images/bg-1C.jpg",
-        "src/assets/Images/bg-1B.jpg",
-      ];
+        "../../assets/Images/bg-1A.jpg",
+        "../../assets/Images/bg-1C.jpg",
+        '../../assets/Images/bg-1B.jpg'
+    ];
+    
     const [img1, img2, img3] = images;
     return (
         <>
-            <BgImg img={img1} i={0} />
-            <BgImg img={img2} i={1} />
-            <BgImg img={img3} i={2} />
+            <BgImg img={logo1} i={0} />
+            <BgImg img={logo3} i={1} />
+            <BgImg img={logo2} i={2} />
            
         </>
        
