e7c677b1140728f6c47944935f92fc22bda5de82
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2024-09-23 10:10:01 +0100

    updated faq section

--- src/components/pages/faqs/Faqs.jsx
+++ src/components/pages/faqs/Faqs.jsx
@@ -9,7 +9,7 @@ const Faqs = () => {
   return (
     <section
       data-testid="faqs"
-      className="bg-[var(--white)] m-6 p-6 flex flex-col gap-[.2rem] mt-24 max-w-[700px] mx-auto"
+      className="w-full bg-[var(--white)] m-6 p-6 flex flex-col gap-[.2rem] mt-24 max-w-[700px] mx-auto"
     >
       <h3 className="text-center mb-8 font-bold text-[25px]">
         Frequently Asked Questions
