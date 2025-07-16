8e213ab2d7540f6840bd3995cb4d8adaf6ad8274
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2025-07-16 23:03:07 +0100

    updated sign in form

--- src/components/pages/signIn/index.tsx
+++ src/components/pages/signIn/index.tsx
@@ -75,7 +75,7 @@ const SignIn: FC<{
   };
 
   return (
-    <section className="bg-[var(--white)] lg:max-w-lg lg:mx-auto h-[500px] md:h-[62%] flex flex-col absolute bottom-0 w-full p-6 transition-all duration-[1s] md:w-[55%] md:top-1/2 md:left-1/2 md:[transform:translate(-50%,-50%)] md:mx-auto md:my-0">
+    <section className="bg-[var(--white)] lg:max-w-lg lg:mx-auto h-[500px] md:h-[62%] max-h-[450px] flex flex-col absolute bottom-0 w-full p-6 transition-all duration-[1s] md:w-[55%] md:top-1/2 md:left-1/2 md:[transform:translate(-50%,-50%)] md:mx-auto md:my-0">
       <CloseIcon
         className="absolute right-1 top-2 cursor-pointer"
         onClick={() => dispatch(closeEntry())}
@@ -95,7 +95,7 @@ const SignIn: FC<{
           setIsSuccessful(false);
           setErrors("");
         }}
-        className="flex flex-col gap-y-4 text-sm max-w-lg mt-4 mx-auto w-full"
+        className="flex flex-col gap-y-3 text-sm max-w-lg mt-4 mx-auto w-full"
       >
         <Input
           label="Email"
